385cfa890d051b39a8e89d5cfea548fcb0f6e6a2
anders <anders.jipwouo@gmail.com>
2024-10-26 13:49:36 +0200

    modify  user entity and create user table.

---
+++ database/seeders/user_seeder.ts
@@ -0,0 +1,11 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import User from '#models/user'
+
+export default class extends BaseSeeder {
+  async run() {
+    await User.create({
+      email: 'anders.jipwouo@gmail.com',
+      password: 'secret',
+    })
+  }
+}
